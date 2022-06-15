22fef26b6a271d0a31c4474493a7e126e7fa92ae
Shaw Lu <shawlu95@gmail.com>
2022-06-14 21:59:00 -0700

    Update transaction delete event to include full details

--- src/events/transaction-deleted-event.ts
+++ src/events/transaction-deleted-event.ts
@@ -1,8 +1,13 @@
 import { Subjects } from './subjects';
+import { Entry } from '../interfaces/entry';
 
 export interface TransactionDeletedEvent {
   subject: Subjects.TransactionDeleted;
   data: {
     id: string;
+    userId: string;
+    memo: string;
+    date: Date;
+    entries: Entry[];
   };
 }
