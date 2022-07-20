c8e305406c512bf00f77a77a23b8618096e615e6
Shaw Lu <shawlu95@gmail.com>
2022-07-20 07:28:00 -0700

    Support google session

--- src/middlewares/current-user.ts
+++ src/middlewares/current-user.ts
@@ -22,15 +22,26 @@ export const currentUser = (
   next: NextFunction
 ) => {
   // Equivalent to (!req.session || !req.session.jwt)
-  if (!req.session?.jwt) {
+  if (!req.session?.jwt && !req.session?.passport?.user?.jwt) {
     return next();
   }
 
   try {
-    const payload = jwt.verify(
-      req.session.jwt,
-      process.env.jwt!
-    ) as UserPayload;
+    var payload;
+    if (req.session?.jwt) {
+      payload = jwt.verify(req.session.jwt, process.env.jwt!) as UserPayload;
+    }
+
+    if (req.session?.passport?.user?.jwt) {
+      payload = jwt.verify(
+        req.session?.passport?.user?.jwt,
+        process.env.jwt!
+      ) as UserPayload;
+    }
+
+    console.log('adhoc sess', req.session?.jwt);
+    console.log('google sess', req.session?.passport?.user?.jwt);
+    console.log('payload', payload);
     req.currentUser = payload;
   } catch (err) {}
   next();
