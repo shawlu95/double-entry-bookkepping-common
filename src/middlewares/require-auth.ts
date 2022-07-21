d7b880ee76d6c87dfb354c13fa891146cd4c999a
Shaw Lu <shawlu95@gmail.com>
2022-07-21 00:28:09 -0700

    Check req.user from passport.js. Deprecate current-user middleware

--- src/middlewares/require-auth.ts
+++ src/middlewares/require-auth.ts
@@ -1,12 +1,26 @@
 import { Request, Response, NextFunction } from 'express';
 import { NotAuthorizedError } from '../errors/not-authorized-error';
 
+interface UserPayload {
+  id: string;
+  email: string;
+  iat: number;
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: UserPayload;
+    }
+  }
+}
+
 export const requireAuth = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  if (!req.currentUser) {
+  if (!req.user) {
     throw new NotAuthorizedError();
   }
   next();
